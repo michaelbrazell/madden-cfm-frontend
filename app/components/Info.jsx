a13b07c6f0b5159f4dae535467a50ceab5ab2220
Mike Murray <michaelbrazell@gmail.com>
2016-12-10 12:14:04 -0500

    Loop through a temporary data set

--- app/components/Info.jsx
+++ app/components/Info.jsx
@@ -1,25 +1,62 @@
 var React = require('react');
 
-var LeagueInfo = (props) => {
-  return (
-    <div>
-      <h1>Main App Rendered</h1>
-      <p>This is the main.jsx component.  This page is going to have:L</p>
-      <ul>
-        <li>Team overview divided up by conference</li>
-        <li>List of user teams</li>
-      </ul>
-      <p>To do:</p>
-      <ul>
-        <li>Restructure this page so that it has a Nav component</li>
-        <li>Restructure so that the standings page belongs to this component
-          <ul>
-            <li>Fix app.jsx so that it doesn't have to require standings, once standings is pulled in as a prop of Main.jsx</li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  );
-}
+var LeagueInfo = React.createClass({
+  render: function() {
+    var teamInfo = [
+    {
+      "abbrName" : "BAL",
+      "cityName" : "Baltimore",
+      "defScheme" : 11,
+      "displayName" : "Ravens",
+      "divName" : "AFC North",
+      "injuryCount" : 4,
+      "logoId" : 24,
+      "nickName" : "Ravens",
+      "offScheme" : 0,
+      "ovrRating" : 79,
+      "primaryColor" : 4985505,
+      "secondaryColor" : 0,
+      "teamId" : 757727261,
+      "userName" : ""
+    },
+    {
+      "abbrName" : "PIT",
+      "cityName" : "Pittsburgh",
+      "defScheme" : 11,
+      "displayName" : "Steelers",
+      "divName" : "AFC North",
+      "injuryCount" : 4,
+      "logoId" : 28,
+      "nickName" : "Steelers",
+      "offScheme" : 1,
+      "ovrRating" : 83,
+      "primaryColor" : 16763904,
+      "secondaryColor" : 0,
+      "teamId" : 757727265,
+      "userName" : ""
+    }
+    ];
+    var htmlText = '';
+    for ( var key in teamInfo ) {
+      htmlText += '<p class="p-name"> Name: ' + teamInfo[key].abbrName + '</p>';
+      htmlText += '<p class="p-loc"> Location: ' + teamInfo[key].cityName + '</p>';
+      htmlText += '<p class="p-desc"> Overall: ' + teamInfo[key].ovrRating + '</p>';
+    }
+    return (
+      <div>
+        <h1>League Info</h1>
+        <p>To do:</p>
+        <ul>
+          <li>Loop through teams from leagueTeamInfo.json</li>
+        </ul>
+        <h2>Teams</h2>
+        { htmlText }
+
+
+
+      </div>
+    );
+  }
+});
 
 module.exports = LeagueInfo;
